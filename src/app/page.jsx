a6e00e6d1f972a44aa226136021e7f862e80385a
Manish-Code-Craft <kumarmanish.amr@gmail.com>
2026-01-22 22:23:50 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,12 +9,12 @@ export default function HomePage() {
     
      <HeroBanner/>
      <ServicesSection/>
-      <div className="py-[200px]">
+      {/* <div className="py-[200px]">
       <h1 className="font-bold text-[60px] md:text-[150px] text-green-600 text-center ">Sysgenix</h1>
      </div>
       <div className="py-[200px]">
       <h1 className="font-bold text-[60px] md:text-[150px] text-red-600 text-center ">Sysgenix</h1>
-     </div>
+     </div> */}
     </>
   );
 }
